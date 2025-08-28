2a39b0fef8e3acd5f45a5d92361afa68df3d9731
Harish Chinthakuntla <harish.chinthakuntla@bizacuity.local>
2025-08-28 17:16:14 +0530

    Deploy Process

--- demoWeb SpringBoot/src/main/java/com/example/demoWeb/Repo/Repopassenger.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoWeb.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demoWeb.model.Passengerdetails;
-
-public interface Repopassenger extends JpaRepository<Passengerdetails, String> {
-
-}
--- demoWeb SpringBoot/src/main/java/com/example/demoWeb/Repo/Supplementpassengerrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoWeb.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demoWeb.model.Supplementpassengers;
-
-public interface Supplementpassengerrepo  extends JpaRepository<Supplementpassengers, String> {
-
-}
